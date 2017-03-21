aea82dc1fe37a067baaa5b2ac306de5753606a4b
TerryLiou <b9503081@gmail.com>
2017-03-21 19:39:54 +0800

    The TidesChartViewController be created

---
+++ Tides/TidesChartViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TidesChartViewController.swift
+//  Tides
+//
+//  Created by 劉洧熏 on 2017/3/21.
+//  Copyright © 2017年 劉洧熏. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TidesChartViewController: UIViewController {
+
+}
