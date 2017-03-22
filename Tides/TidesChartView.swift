56507852a58434c7a9ae3cb4080ced740a9e00a5
TerryLiou <b9503081@gmail.com>
2017-03-22 10:51:13 +0800

    2017.03.22 before work commit

---
+++ Tides/TidesChartView.swift
@@ -0,0 +1,14 @@
+//
+//  TidesChartView.swift
+//  Tides
+//
+//  Created by 劉洧熏 on 2017/3/22.
+//  Copyright © 2017年 劉洧熏. All rights reserved.
+//
+
+import Foundation
+import Charts
+
+class TidesChartView: LineChartView {
+    
+}
---
+++ Tides/TidesDataMobel.swift
@@ -0,0 +1,9 @@
+//
+//  TidesDataMobel.swift
+//  Tides
+//
+//  Created by 劉洧熏 on 2017/3/22.
+//  Copyright © 2017年 劉洧熏. All rights reserved.
+//
+
+import Foundation
