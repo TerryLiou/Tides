0d83375ac886b1446ff3071396e23e1ccc8a945d
TerryLiou <b9503081@gmail.com>
2017-03-26 13:01:01 +0800

    BuildTidesTableView

---
+++ Tides/TidesSearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TidesSearchTableViewCell.swift
+//  Tides
+//
+//  Created by 劉洧熏 on 2017/3/26.
+//  Copyright © 2017年 劉洧熏. All rights reserved.
+//
+
+import UIKit
+
+class TidesSearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Tides/TidesSearchTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TidesSearchController.swift
+//  Tides
+//
+//  Created by 劉洧熏 on 2017/3/26.
+//  Copyright © 2017年 劉洧熏. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TidesSearchTableViewController: UITableViewController {
+    
+}
