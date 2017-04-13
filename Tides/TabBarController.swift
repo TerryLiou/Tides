e55338d3b63eb3f2eef1b1774871e55b81b01f00
TerryLiou <b9503081@gmail.com>
2017-04-13 17:42:00 +0800

    Build tabbarController Enum and upload icon

--- Tides/TabBarController.swift
+++ Tides/TabBarController.swift
@@ -8,6 +8,62 @@
 
 import UIKit
 
+enum TarBarItemTypes: Int {
+
+    case chart, search, weather, moonAge
+
+    var item: UITabBarItem {
+
+        return UITabBarItem(title: title, image: image, selectedImage: rawValue)
+
+    }
+
+    var title: String {
+
+        switch self {
+
+        case .chart:
+
+            return "潮汐資訊"
+
+        case .search:
+
+            return "潮汐查詢"
+
+        case .weather:
+
+            return "天氣資訊"
+
+        case .moonAge:
+
+            return "月齡資訊"
+
+        }
+    }
+
+    var image: UIImage {
+
+        switch self {
+
+        case .chart:
+
+            return #imageLiteral(resourceName: "tides").withRenderingMode(.alwaysOriginal)
+
+        case .search:
+
+            return #imageLiteral(resourceName: "search").withRenderingMode(.alwaysOriginal)
+
+        case .weather:
+
+            return #imageLiteral(resourceName: "weather").withRenderingMode(.alwaysOriginal)
+
+        case .moonAge:
+
+            return #imageLiteral(resourceName: "moon").withRenderingMode(.alwaysOriginal)
+
+        }
+    }
+}
 class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
